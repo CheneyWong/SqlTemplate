3a243c197eb1f70abe4cd5134b51c4c91e069902
Cheney <hq@gpio.me>
2016-08-29 09:45:01 +0800

    Bindings 更好用了

--- src/main/java/club/fullstack/sqltemplate/Bindings.java
+++ src/main/java/club/fullstack/sqltemplate/Bindings.java
@@ -3,15 +3,17 @@ package club.fullstack.sqltemplate;
 import java.util.HashMap;
 
 public class Bindings extends HashMap<Object,Object>{
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -7290846439659491933L;
 
-	public Bindings bind(Object key , Object value ){
-		this.put(key, value) ;
-		
+	public Bindings(){}
+
+	public Bindings(Object key , Object value) {
+		this();
+		this.put(key, value);
+	}
+
+	public Bindings bind(Object key , Object value){
+		this.put(key, value);
 		return this; 
 	}
 
